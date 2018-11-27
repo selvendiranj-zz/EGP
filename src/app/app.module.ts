65bdaf235e37aaaf496b4548b8a8b3a722e14f7e
Selvendiran Jayaraman <selvendiranj@hotmail.com>
2018-11-27 19:42:07 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        MDBBootstrapModule.forRoot(),
+        BrowserModule,
+        AppRoutingModule
+    ],
+    schemas: [ NO_ERRORS_SCHEMA ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
